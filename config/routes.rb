aa4e08ab613a2479153f59947b808936bf0cad08
QuenMBar <Qbeast360@gmail.com>
2021-05-14 00:33:46 -0700

    Made notes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails
     .application
     .routes
     .draw do
-        resources :summoners do
-            get 'new_challenges', :show
-            get 'all_challenges', :show
-        end
+        resources :summoners, only: %i[index show]
 
         # resources :challenges
-        resources :created_challenges
-        resources :league_queues
-        resources :summoner_spells
+        resources :created_challenges, only: %i[show create destroy update]
+        resources :league_queues, only: %i[show]
+        resources :summoner_spells, only: %i[show]
         # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     end
